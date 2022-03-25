afdb7cc0761cb7c521d10fe04babd7ef190c72e8
Kazuma Kanno <zeal21300066@fsgcl.jp>
2022-03-25 12:30:39 +0900

    Playerの変更・アニメーションの追加

---
+++ GameSources/GameStage.h
@@ -0,0 +1,28 @@
+﻿/*!
+@file GameStage.h
+@brief �Q�[���X�e�[�W
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross { 
+	//--------------------------------------------------------------------------------------
+	// ゲームステージクラス
+	//--------------------------------------------------------------------------------------
+	class GameStage : public Stage {
+	//CSVファイルそのC
+	CsvFile m_CsvC;
+	//ビューの作成
+	void CreateViewLight();
+	void CreatestageObject();
+	public:
+	//構築と破棄
+	GameStage() :Stage() {}
+	virtual ~GameStage() {}
+	//初期化
+	virtual void OnCreate()override;
+	};
+}
+//end basecross
+
