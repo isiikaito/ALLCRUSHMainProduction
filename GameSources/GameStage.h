18f4d788be5a3efc74adb4cb37fd01861c3f954e
HibikiEndou <zeal21300054@fsgcl.jp>
2022-03-28 11:22:30 +0900

    コン不リフトの修正

--- GameSources/GameStage.h
+++ GameSources/GameStage.h
@@ -6,23 +6,22 @@
 #pragma once
 #include "stdafx.h"
 
-namespace basecross { 
+namespace basecross {
 	//--------------------------------------------------------------------------------------
 	// ゲームステージクラス
 	//--------------------------------------------------------------------------------------
 	class GameStage : public Stage {
-	//CSVファイルそのC
-	CsvFile m_CsvC;
-	//ビューの作成
-	void CreateViewLight();
-	void CreatestageObject();
+		//CSVファイルそのC
+		CsvFile m_CsvC;
+		//ビューの作成
+		void CreateViewLight();
+		void CreatestageObject();
 	public:
-	//構築と破棄
-	GameStage() :Stage() {}
-	virtual ~GameStage() {}
-	//初期化
-	virtual void OnCreate()override;
+		//構築と破棄
+		GameStage() :Stage() {}
+		virtual ~GameStage() {}
+		//初期化
+		virtual void OnCreate()override;
 	};
 }
 //end basecross
-
