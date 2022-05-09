934cfe262c00a1504106aa6916b524bc939fe2c3
Ayumu Mori <zeal21300088@fsgcl.jp>
2022-05-09 15:36:04 +0900

    テスト

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -7,6 +7,66 @@
 #include "Project.h"
 
 namespace basecross {
+#pragma comment(lib, "Effekseer.lib")
+#pragma comment(lib, "EffekseerRendererDX11.lib")
+
+	Player::Player(const shared_ptr<Stage>& StagePtr) :
+		GameObject(StagePtr),
+		MaxMoveSpeed(6.0f),
+		moveStop(1.0f),	
+		moveDir(0.0f, 0.0f, 0.0f),
+		speed(0.0f),
+		accel(0.0f),
+		itemCount(0.0f),
+		m_TotalTime(0.0f), m_isPlay(false), m_handle(0),
+		m_manager(nullptr), m_renderer(nullptr), m_effect(nullptr)
+	{}
+
+	void Player::CreateEffect() {
+		auto d3D11Device = App::GetApp()->GetDeviceResources()->GetD3DDevice();
+		auto d3D11DeviceContext = App::GetApp()->GetDeviceResources()->GetD3DDeviceContext();;
+		// エフェクトのレンダラーの作成
+		m_renderer = ::EffekseerRendererDX11::Renderer::Create(d3D11Device, d3D11DeviceContext, 8000);
+
+
+		// エフェクトのマネージャーの作成
+		m_manager = ::Effekseer::Manager::Create(8000);
+		// 描画モジュールの設定
+		m_manager->SetSpriteRenderer(m_renderer->CreateSpriteRenderer());
+		m_manager->SetRibbonRenderer(m_renderer->CreateRibbonRenderer());
+		m_manager->SetRingRenderer(m_renderer->CreateRingRenderer());
+		m_manager->SetTrackRenderer(m_renderer->CreateTrackRenderer());
+		m_manager->SetModelRenderer(m_renderer->CreateModelRenderer());
+
+		// テクスチャ、モデル、カーブ、マテリアルローダーの設定する。
+		// ユーザーが独自で拡張できる。現在はファイルから読み込んでいる。
+		m_manager->SetTextureLoader(m_renderer->CreateTextureLoader());
+		m_manager->SetModelLoader(m_renderer->CreateModelLoader());
+		m_manager->SetMaterialLoader(m_renderer->CreateMaterialLoader());
+		m_manager->SetCurveLoader(Effekseer::MakeRefPtr<Effekseer::CurveLoader>());
+
+		// 視点位置を確定
+		auto g_position = ::Effekseer::Vector3D(10.0f, 5.0f, 20.0f);
+
+		// 投影行列を設定
+		float w = (float)App::GetApp()->GetGameWidth();
+		float h = (float)App::GetApp()->GetGameHeight();
+		m_renderer->SetProjectionMatrix(::Effekseer::Matrix44().PerspectiveFovRH(
+			90.0f / 180.0f * 3.14f, w / h, 1.0f, 500.0f));
+		// カメラ行列を設定
+		m_renderer->SetCameraMatrix(
+			::Effekseer::Matrix44().LookAtRH(g_position, ::Effekseer::Vector3D(0.0f, 0.0f, 0.0f), ::Effekseer::Vector3D(0.0f, 1.0f, 0.0f)));
+
+		wstring dataDir;
+		App::GetApp()->GetDataDirectory(dataDir);
+		dataDir += L"effect\\";
+		//wstring wstrEfk = dataDir + L"Laser01.efk";
+		wstring wstrEfk = dataDir + L"BrakeSmoke.efkefc";
+
+		m_effect = ::Effekseer::Effect::Create(m_manager, (const char16_t*)wstrEfk.c_str());
+
+	}
+
 	void Player::OnCreate()
 	{
 
@@ -53,6 +113,9 @@ namespace basecross {
 		ptrDraw->AddAnimation(L"ActionPull", 40, 20, false, 35.0f);
 		ptrDraw->AddAnimation(L"ActionPush", 60, 15, false, 35.0f);
 		ptrDraw->ChangeCurrentAnimation(L"Default");
+
+		CreateEffect();
+
 	}
 
 	void Player::OnUpdate()
@@ -168,7 +231,13 @@ namespace basecross {
 					auto ctrlVec = App::GetApp()->GetInputDevice().GetControlerVec();
 					if (ctrlVec[0].wButtons & XINPUT_GAMEPAD_A) {
 						//コントローラのボタンが押されていたら、shPtrを消す
+						auto ptr = dynamic_pointer_cast<Wall>(shPtr);
 						GetStage()->RemoveGameObject<Wall>(shPtr);
+						if (!m_isPlay) {
+							auto pos = ptr->GetComponent<Transform>()->GetWorldPosition();
+							m_handle = m_manager->Play(m_effect, pos.x, pos.y, pos.z);
+							m_isPlay = true;
+						}
 					}
 				}
 			}
@@ -275,6 +344,33 @@ namespace basecross {
 		}
 
 	}
+	void Player::OnDraw() {
+		GameObject::OnDraw();
+		if (m_isPlay) {
+			auto elps = App::GetApp()->GetElapsedTime();
+			m_TotalTime += elps;
+			if (m_TotalTime >= 2.0f) {
+				m_manager->StopEffect(m_handle);
+				m_TotalTime = 0.0f;
+				m_isPlay = false;
+				return;
+			}
+			else {
+				// マネージャーの更新
+				m_manager->Update();
+				// 時間を更新する
+				m_renderer->SetTime(elps);
+				// エフェクトの描画開始処理を行う。
+				m_renderer->BeginRendering();
+				// エフェクトの描画を行う。
+				m_manager->Draw();
+				// エフェクトの描画終了処理を行う。
+				m_renderer->EndRendering();
+			}
+
+		}
+	}
+
 	void Player::OnDestroy() {
 		//BGMのストップ
 		auto PtrXA = App::GetApp()->GetXAudio2Manager();
